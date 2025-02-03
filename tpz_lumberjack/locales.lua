e79746e97640dd8ff638ed7b8f496cf8112cd6f5
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-02-03 03:48:12 +0200

    Update locales.lua

--- tpz_lumberjack/locales.lua
+++ tpz_lumberjack/locales.lua
@@ -2,11 +2,17 @@ Locales = {}
 
 Locales = {
 
-    ['HATCHET']              = "Start Chopping Tree",
-    ['HATCHET_NAME']         = "Hatchet",
-    ['CANNOT_CARRY']         = "You don't have enough space to carry more items.",
-    ['FOUND_NOTHING']        = "~e~You haven't found anything",
+    ['HATCHET']                               = "Chopp Tree",
+    ['HATCHET_NAME']                          = "Hatchet",
+    ['CANNOT_CARRY']                          = "You don't have enough space to carry more items.",
+    ['FOUND_NOTHING']                         = "~e~You haven't found anything",
 
-    ['NO_DURABILITY']        = "~e~The hatchet you have been using, unfortunately seems to be useless.",
-    ['SUCCESSFULLY_FOUND']   = "You have found ",
+    ['NO_DURABILITY']                         = "~e~The hatchet you have been using, unfortunately seems to be useless.",
+    ['SUCCESSFULLY_FOUND_REWARDS']            = "You have found %s",
+
+    ['NOT_REQUIRED_JOB']                      = "~e~You don't have the required job.",
+    ['ALREADY_CHOPPED']                       = "~e~The specified tree has already been chopped.",
+
+    ['DEVTOOLS_INJECTION_DETECTED_TITLE_LOG'] = "DevTools / Injection Cheat Found!",
+    ['DEVTOOLS_INJECTION_DETECTED']           = "You have been kicked due to cheating by using DevTools or Injection cheat.",
 }
