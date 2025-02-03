ba05fc063dd13eabe38e9ba944aabd190f9304c3
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-02-03 03:49:14 +0200

    Update fxmanifest.lua

--- tpz_lumberjack/fxmanifest.lua
+++ tpz_lumberjack/fxmanifest.lua
@@ -2,13 +2,18 @@ fx_version "adamant"
 games {"rdr3"}
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-version '1.0.0'
-
 author 'Nosmakos'
-description 'TPZ-CORE Lumberjack'
+description 'TPZ-CORE - Lumberjack'
+version '1.0.0'
 
 shared_scripts { 'config.lua', 'config_trees.lua', 'locales.lua' }
 server_scripts { 'server/*.lua' }
 client_scripts { 'client/*.lua' }
+
+dependencies {
+    'tpz_core',
+    'tpz_characters',
+    'tpz_inventory',
+}
 
 lua54 'yes'
