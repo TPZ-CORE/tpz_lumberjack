530c42336dabb61be362c718483945e6a0edce07
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2026-09-20 17:26:26 +0300

    Fix notification message for hatchet item use

--- tpz_lumberjack/server/tp-server_items.lua
+++ tpz_lumberjack/server/tp-server_items.lua
@@ -8,7 +8,7 @@ TPZInv.registerUsableItem(Config.HatchetItem, "tpz_lumberjack", function(data)
 	local _source = data.source
 
 	if Config.Durability.Enabled and data.durability <= 0 then
-		SendNotification(_source, Locales['NOT_DURABILITY'])
+		SendNotification(_source, Locales['NO_DURABILITY'])
 		return
 	end
 
